9fea64d535446c757371bcdea16edf37f60f6197
abbassaid <abbas-s-7@hotmail.com>
2023-10-05 13:07:33 -0400

    Fixing Output for Question 3 add.js

--- question-3/add.js
+++ question-3/add.js
@@ -12,7 +12,7 @@ if (!fs.existsSync(directory)) {
 process.chdir(directory)
 
 // Creating 10 log files and writing some text into the files
-for (let i = 1; i <= 10; i++) {
+for (let i = 0; i < 10; i++) {
     const fileName = `log${i}.txt`
     const filePath = path.join(directory, fileName)
     const text = `log file ${i}.`
